3db421c8a0b78a0f830b06ed70f7ca06f20a48fb
饶家俊 <471412450@qq.com>
2021-02-22 17:52:06 +0800

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,58 @@
-import logo from './logo.svg';
-import './App.css';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header className="App-header">
+          <ul>
+            <li>
+              <Link to="/my">my</Link>
+            </li>
+            <li>
+              <Link to="/project">project</Link>
+            </li>
+            <li>
+              <Link to="/todo">todo</Link>
+            </li>
+          </ul>
+
+          <Switch>
+            <Route path="/my">
+              <My />
+            </Route>
+            <Route path="/project">
+              <Project />
+            </Route>
+            <Route path="/todo">
+              <Todo />
+            </Route>
+          </Switch>
+        </header>
+      </div>
+    </Router>
   );
+}
+
+const My = ()=>{
+  return (
+    <div>My</div>
+  )
+}
+const Project = ()=>{
+  return (
+    <div>project</div>
+  )
+}
+const Todo = ()=>{
+  return (
+    <div>Todo</div>
+  )
 }
 
 export default App;
